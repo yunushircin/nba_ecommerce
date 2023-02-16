92c72a2ed79916d7f98b548c1eefd1723f2a1bb2
yunushircin <yunushircin@gmail.com>
2023-02-16 03:45:07 +0300

    basepy

--- core/settings/base.py
+++ core/settings/base.py
@@ -114,7 +114,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 MEDIA_URL = '/images/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
